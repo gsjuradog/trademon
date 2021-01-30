c72ea7e6b8e869d5a061b25d8013b2ba10ec53e5
gsjuradog <gsjuradog@unal.edu.co>
2021-01-30 15:13:17 +0100

    Added Graphql typeDefs

--- server/controllers/resolvers.js
+++ server/controllers/resolvers.js
@@ -1,15 +1,30 @@
+const testObj = {
+  username: 'Pikachu',
+  trainerID: 4654654,
+  trainerName: 'Bernie',
+  mtgoID: 'ghfghfghgh',
+  mtgoName: 'ghfghdfhgfghh',
+  latitude: 46546,
+  longitude: 4546,
+  buyerRating: 0,
+  numBuyRatings: 0,
+  sellerRating: 0,
+  numSellRatings: 0,
+  numOfStrikes: 0,
+};
+
 const Query = {
   user: (root, args, context) => {
     console.log('user');
-    return 'user';
-  }
-}
+    return testObj;
+  },
+};
 
 const Mutation = {
   editUser: (root, args, context) => {
     console.log('edit user');
     return 'edit user';
-  }
-}
+  },
+};
 
 module.exports = { Query, Mutation };
