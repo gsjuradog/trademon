a4fcc0e13b33d27ac25ddda626fb595a7810c49a
Dan <dan.bayford@gmail.com>
2021-01-31 17:56:53 +0000

    feat: create GraphQL resolvers and typedefs

--- server/controllers/resolvers.js
+++ server/controllers/resolvers.js
@@ -1,3 +1,5 @@
+const db = require('../models/index');
+
 const testObj = {
   username: 'Pikachu',
   trainerID: 4654654,
@@ -13,24 +15,87 @@ const testObj = {
   numOfStrikes: 1,
 };
 
+// query ($user: String!){
+//   getUserData(user: $user) {
+//     numOfStrikes
+//     trainerID
+//     username {
+//       username 
+//     }
+//   }
+// }
+
+// Query Variable: { "user": "Dan"}
+
+//GraphQL Queries
 const Query = {
-  user: (root, args, context) => {
-    console.log('user');
-    return testObj;
+  getUserData: (root, args, context) => {
+    const { user } = args;
+    const query = getUser(user);
+    return query;
   },
 
   getPrivateChat: (root, args, context ) => {
-    
-    return testObj.privateChat
+    const { chatID } = args;
+    const query = getChat(chatID);
+    return query;
+  },
+
+  getTrade: (root, args, context) => {
+    const { trade } = args;
+    const query = getTrade(trade);
+    return query;
   }
 
 };
 
+//Resolvers for GraphQL Queries
+const getUser = async (user) => {
+  const reply = await db.UserData.findOne({where: {
+    username:user
+  }});
+  return reply;
+}
+
+const getChat = async (chat) => {
+  //chat database interaction
+  return 'Chat history';
+}
+
+const getTrade = async (trade) => {
+  //trade database interaction
+  return 'Trade'
+}
+
+//GraphQL Mutations
 const Mutation = {
   editUser: (root, args, context) => {
-    console.log('edit user');
     return 'edit user';
   },
 };
 
-module.exports = { Query, Mutation };
+//Resolvers for GraphQL Mutations
+
+
+//Type Resolvers
+const UserData = {
+  username: (username) => {return username}
+  //need to define privatechat resolver
+}
+
+const PrivateChat = {
+  //need to define history resolver
+}
+
+const Message = {
+  //need to define from & to resolvers
+}
+
+const Trade = {
+  //need to define tradeItemVariableData resolver
+  //need to define offerItemVariableData resolver
+}
+
+
+
+module.exports = { Query, Mutation, UserData, PrivateChat, Message, Trade };
