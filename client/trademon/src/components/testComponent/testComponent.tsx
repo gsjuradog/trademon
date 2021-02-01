b1805f36ca3f4a11a60c029740c57a5543461a50
Wladimir <oksana.abdulayeva@ewetel.net>
2021-02-01 08:55:43 +0100

    refactor(redux section): modern redux with Redux ToolKit
    BREAKING CHANGE: npm install @reduxjs/toolkit
    
    fix #25

--- client/trademon/src/components/testComponent/testComponent.tsx
+++ client/trademon/src/components/testComponent/testComponent.tsx
@@ -1,34 +1,20 @@
 import React from 'react';
-
-//Redux imports
-import { useDispatch, useSelector } from "react-redux";
-import { testAction } from '../../redux/actions/actions';
-import MiniTileContainer from '../containerComponents/miniTileContainer'
+import MiniTileContainer from '../containerComponents/miniTileContainer';
+import ReduxTest from '../testComponents/reduxTestComponent';
+import ReduxTestList from '../testComponents/reduxTestListComponent';
 
 import './testComponent.scss';
 
 const TestComponent = () => {
-
-  const dispatch = useDispatch();
-
-  //To get hold of current state from store, use useSelector hook
-  const testState = useSelector((state:any) => state.testReducer);
-
-  const toggle = () => {
-    //const action = callAction(1);
-    //dispatch(action);
-    dispatch(testAction(1));  //<-- Payload sent to action, and then to reducer
-  }
-
   return (
     <div>
       <h1>Test Component</h1>
-      <h3>Test State: {testState}</h3>
-      <button onClick={toggle}>CLICK ME!</button>
-      <MiniTileContainer/>
-
+      <ReduxTest />
+      <ReduxTestList />
+      <hr />
+      <MiniTileContainer />
     </div>
-  )
-}
+  );
+};
 
 export default TestComponent;
--- client/trademon/src/redux/actions/actions.ts
+++
@@ -1,6 +0,0 @@
-export const testAction = (value:Number) => {
-  return {
-    type: "TEST",
-    payload: value
-  };
-}
