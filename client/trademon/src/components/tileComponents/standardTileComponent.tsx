60a6f655f4d18df4ae1d68c9b808667939cf52e2
gsjuradog <gsjuradog@unal.edu.co>
2021-02-02 18:15:04 +0100

    Fetch data to edit tiles

--- client/trademon/src/components/tileComponents/standardTileComponent.tsx
+++ client/trademon/src/components/tileComponents/standardTileComponent.tsx
@@ -2,27 +2,46 @@ import React from 'react';
 import '../../styling/tiles.scss';
 import UserRatingComponent from '../ratingComponents/userRatingComponent';
 
-export default function StandardTile() {
+interface trades {
+  tradeID:number,
+  numViews:number, 
+  seller:string, 
+  pokeNum:number, 
+  pokeName:string, 
+  pokeGen:number, 
+  pokeLvl:number, 
+  fastMove:string, 
+  chargeMove:string, 
+  isShiny:boolean ,
+  appraisal:number, 
+  price:number, 
+  tax:number
+}
+type trade = { 
+  trade: trades
+} 
+
+export default function StandardTile({trade}:trade) {
   return (
     <div className="standard-tile-container">
       <div className="title-row">
         <span className="spacer-span"></span>
-        <div className="std-tile-title-text">Charmeleon</div>
+        <div className="std-tile-title-text">{trade.pokeName}</div>
         <img src={'/assets/FavIconEmpty.png'} className="heart"></img>
       </div>
       <div className="sprite-row">
         <img className="standard-sprite" src={'/assets/testSprite.png'}></img>
       </div>
-      <div className="standard-info-row">
-        <p>CP: 2300</p>
+      <div className="standard-info-row"> 
+        <p>CP: {trade.pokeLvl}</p>
         <UserRatingComponent />
       </div>
       <div className="seller-row">
         <div className="seller-info">
           <img className="standard-avatar" src={'/assets/avatarIcon.png'}></img>
-          <p className="seller-text">Seller123</p>
+          <p className="seller-text">{trade.seller}</p>
         </div>
-        <p>$1</p>
+        <p>${trade.price}</p>
       </div>
     </div>
   );
