d7fb320c215c10f2979468992cb775c760d7ef21
Dan <dan.bayford@gmail.com>
2021-02-02 19:21:19 +0000

    feat: css refactor

--- client/trademon/src/components/navComponents/searchComponents/searchBarComponent.tsx
+++ client/trademon/src/components/navComponents/searchComponents/searchBarComponent.tsx
@@ -5,25 +5,29 @@ import ProfileOverlay from './profileOverlayComponent'
 export default function SearchBar() {
   return (
     <div className="nav-container">
+
       <a href={'/'}>
-        <div className="logo-box">
-          <img className="logo-img" src={'/assets/trademon-logo.png'}/>
-          <div className="platform-text">trademon.io</div>
-        </div>
+            <img className="logo-img" src={'/assets/trademon-logo.png'}/>
       </a>
-      <div className="nav-row">
-        <div className="filters-box">
-          <select className="select-dropdown">
-            <option value="Pokemon Go">Pokemon Go</option>
-            <option value="MTG: Online">MTG: Online</option>
-          </select>
-          <input 
-            type="text" placeholder="Search offers..." 
-            id="search-field" className="search-field" 
-          />
-        </div>
-      <ProfileOverlay></ProfileOverlay>
+
+      <h1 className="platform-text">trademon.io</h1>
+      
+
+      <div className="filters-box">
+        <select className="select-dropdown">
+          <option value="Pokemon Go">Pokemon Go</option>
+          <option value="MTG: Online">MTG: Online</option>
+        </select>
+        <input 
+          type="text" placeholder="Search offers..." 
+          id="search-field" className="search-field" 
+        />
       </div>
+
+   
+
+      <ProfileOverlay></ProfileOverlay>
+
     </div>
   )
 }
