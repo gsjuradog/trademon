05c0011076fef6967312481892c1c3f27aa56ee6
Dan <dan.bayford@gmail.com>
2021-02-03 12:06:29 +0000

    feat: nav bar mobile response, drawer mobile response

--- client/trademon/src/components/navComponents/searchComponents/searchBarComponent.tsx
+++ client/trademon/src/components/navComponents/searchComponents/searchBarComponent.tsx
@@ -5,11 +5,15 @@ import ProfileOverlay from './profileOverlayComponent'
 export default function SearchBar() {
   return (
     <div className="nav-container">
+
       <a className="logo-img-link" href={'/'} >
         <img className="logo-img" src={'/assets/trademon-logo.png'}/>
       </a>
+
       <div className="search-container-vertical">
+
         <h1 className="platform-text">trademon.io</h1>
+        
         <div className="filters-box">
           <select className="select-dropdown">
             <option value="Pokemon Go">Pokemon Go</option>
@@ -20,6 +24,7 @@ export default function SearchBar() {
             id="search-field" className="search-field" 
           />
         </div>
+
       </div>
       <ProfileOverlay></ProfileOverlay>
 
