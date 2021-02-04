076c25d9f9f5e655a5e8d9cae3a4a969709435ef
Dalton Klein <deklein@live.com>
2021-02-03 19:16:46 -0600

    feat(added dynamic routes for trade offers, accessible through clicking standard tile): fE-Feat
    Added Dynamic Routes For Trade Offers, Accessible Through Clicking Standard Tile
    
    fix #83

--- client/trademon/src/components/tileComponents/standardTileComponent.tsx
+++ client/trademon/src/components/tileComponents/standardTileComponent.tsx
@@ -1,48 +1,37 @@
 import React from 'react';
 import '../../styling/tiles.scss';
 import UserRatingComponent from '../ratingComponents/userRatingComponent';
+import { Trades } from '../../utils/interfaces'
+import { Link } from 'react-router-dom';
 
-interface trades {
-  tradeID:number,
-  numViews:number, 
-  seller:string, 
-  pokeNum:number, 
-  pokeName:string, 
-  pokeGen:number, 
-  pokeLvl:number, 
-  fastMove:string, 
-  chargeMove:string, 
-  isShiny:boolean ,
-  appraisal:number, 
-  price:number, 
-  tax:number
-}
 type trade = { 
-  trade: trades
+  trade: Trades
 } 
 
 export default function StandardTile({trade}:trade) {
   return (
-    <div className="standard-tile-container">
-      <div className="title-row">
-        <span className="heart"></span>
-        <div className="std-tile-title-text">{trade.pokeName}</div>
-        <img src={'/assets/FavIconEmpty.png'} className="heart" alt="Heart Icon"></img>
-      </div>
-      <div className="sprite-row">
-        <img className="standard-sprite" src={'/assets/testSprite.png'} alt={`${trade.pokeName}`}></img>
-      </div>
-      <div className="standard-info-row"> 
-        <p>CP: {trade.pokeLvl}</p>
-        <UserRatingComponent />
-      </div>
-      <div className="seller-row">
-        <div className="seller-info">
-          <img className="standard-avatar" src={'/assets/avatarIcon.png'} alt="avatar icon"></img>
-          <p className="seller-text">{trade.seller}</p>
+    <Link to={`/trade/${trade.tradeID}`}>
+      <div className="standard-tile-container">
+        <div className="title-row">
+          <span className="heart"></span>
+          <div className="std-tile-title-text">{trade.pokeName}</div>
+          <img src={'/assets/FavIconEmpty.png'} className="heart" alt="Heart Icon"></img>
         </div>
-        <p>${trade.price}</p>
+        <div className="sprite-row">
+          <img className="standard-sprite" src={'/assets/testSprite.png'} alt={`${trade.pokeName}`}></img>
+        </div>
+        <div className="standard-info-row"> 
+          <p>CP: {trade.pokeLvl}</p>
+          <UserRatingComponent />
+        </div>
+        <div className="seller-row">
+          <div className="seller-info">
+            <img className="standard-avatar" src={'/assets/avatarIcon.png'} alt="avatar icon"></img>
+            <p className="seller-text">{trade.seller}</p>
+          </div>
+          <p>${trade.price}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
