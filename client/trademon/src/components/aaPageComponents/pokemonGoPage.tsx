603c2f01142b23646294dc7c071e87496d7ba42b
Wilfredo Campos <wilfredo.campos@gmail.com>
2021-02-05 10:12:16 +0100

    refactor(pokemongopage): modify pokemonGoPage to handle dispatch

--- client/trademon/src/components/aaPageComponents/pokemonGoPage.tsx
+++ client/trademon/src/components/aaPageComponents/pokemonGoPage.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import SearchBar from '../navComponents/searchComponents/searchBarComponent'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchTrades } from '../../store/standardTileSlice';
+import SearchBar from '../navComponents/searchComponents/searchBarComponent';
 import SearchResultsContainer from '../containerComponents/searchResultsContainer';
 
 export default function PokemonGoPage() {
+  const world: string = 'Pokemon';
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTrades(world));
+  }, []);
+
   return (
-    <div>
-      <SearchBar></SearchBar>
-      <SearchResultsContainer></SearchResultsContainer>
-    </div>
-  )
+    <>
+      <div>
+        <SearchBar></SearchBar>
+      </div>
+      <div>
+        <SearchResultsContainer
+          key={'Pokemon'}
+          world={world}
+        ></SearchResultsContainer>
+      </div>
+    </>
+  );
 }
