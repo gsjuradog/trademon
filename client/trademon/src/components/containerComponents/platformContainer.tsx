1748e6380486a0ae7d8c240dfab71d22b795c249
gsjuradog <gsjuradog@unal.edu.co>
2021-02-09 15:55:19 +0100

    fix(fe wow and rl tiles): populate wow and rocket
    we populated the two containers with offers
    
    fixed #158

--- client/trademon/src/components/containerComponents/platformContainer.tsx
+++ client/trademon/src/components/containerComponents/platformContainer.tsx
@@ -10,6 +10,82 @@ interface IProps {
   world: string;
 }
 
+const miniTilesWow: MiniTileTrade[] = [
+  {
+    id: 20000,
+    image: 'assets/wow/1.png',
+    name: 'Stormreaver',
+    price: 47.99,
+    world: 'WoW',
+  },
+  {
+    id: 20001,
+    image: 'assets/wow/2.png',
+    name: 'Argent Dawn Lvl 60',
+    price: 37.99,
+    world: 'WoW',
+  },
+  {
+    id: 20002,
+    image: 'assets/wow/3.png',
+    name: 'Account lvl 60',
+    price: 19.99,
+    world: 'WoW',
+  },
+  {
+    id: 20003,
+    image: 'assets/wow/4.png',
+    name: 'Christmas Armor',
+    price: 25.99,
+    world: 'WoW',
+  },
+];
+
+const miniTilesRL: MiniTileTrade[] = [
+  {
+    id: 20000,
+    image: 'assets/RL/r1.png',
+    name: 'Octane ZSR',
+    price: 47.99,
+    world: 'RL',
+  },
+  {
+    id: 20001,
+    image: 'assets/RL/r2.png',
+    name: 'Hotweels skin',
+    price: 27.95,
+    world: 'RL',
+  },
+  {
+    id: 20002,
+    image: 'assets/RL/r3.png',
+    name: 'Komodo',
+    price: 19.99,
+    world: 'RL',
+  },
+  {
+    id: 20003,
+    image: 'assets/RL/r4.png',
+    name: 'Maverick',
+    price: 10.99,
+    world: 'RL',
+  },
+  {
+    id: 20004,
+    image: 'assets/RL/r5.png',
+    name: 'Burnt Sienna',
+    price: 125.99,
+    world: 'RL',
+  },
+  {
+    id: 20005,
+    image: 'assets/RL/r5.png',
+    name: 'Plate lionWeel',
+    price: 7.99,
+    world: 'RL',
+  },
+];
+
 export default function PlatformContainer(props: IProps) {
   const miniTiles = useSelector((state: RootState) => state.trade);
 
@@ -32,24 +108,18 @@ export default function PlatformContainer(props: IProps) {
       ));
       break;
     case 'WoW':
-      miniTilesRender = (
-        <li style={{ listStyleType: 'none' }}>
-          <p style={{ fontSize: '1.5rem', textAlign: 'center' }}>
-            World of Warcraft! <br />
-            ... coming soon &#x1F929;
-          </p>
+      miniTilesRender = miniTilesWow.map((miniTile: MiniTileTrade) => (
+        <li style={{ listStyleType: 'none' }} key={world + miniTile.id}>
+          <MiniTileComponent {...miniTile}></MiniTileComponent>
         </li>
-      );
+      ));
       break;
     case 'RocketLeague':
-      miniTilesRender = (
-        <li style={{ listStyleType: 'none' }}>
-          <p style={{ fontSize: '1.5rem', textAlign: 'center' }}>
-            Rocket League! <br />
-            ... coming soon &#x1F929;
-          </p>
+      miniTilesRender = miniTilesRL.map((miniTile: MiniTileTrade) => (
+        <li style={{ listStyleType: 'none' }} key={world + miniTile.id}>
+          <MiniTileComponent {...miniTile}></MiniTileComponent>
         </li>
-      );
+      ));
       break;
     default:
       break;
